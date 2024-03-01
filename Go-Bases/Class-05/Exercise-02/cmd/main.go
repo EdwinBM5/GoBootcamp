bfadd918688aaa93fb4a89b1a3c0a0a4ac5652cf
EdwinBM5 <edwin.becerra@mercadolibre.com.co>
2024-03-01 12:58:45 -0500

    feat: adding exercises from class 5

---
+++ Go-Bases/Class-05/Exercise-02/cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "class/five/employee/internal"
+
+func main() {
+	person := internal.Person{Name: "Julian", ID: 1, DateOfBirth: "01/05/1993"}
+	employee := internal.Employee{ID: 4, Position: "Software developer", Person: person}
+
+	internal.Employee.PrintEmployee(employee)
+}
